713b275cef8147012e4fc85a108bf14f5f0d6b2f
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2026-03-11 18:38:51 +0000

    third derivative

--- differentials.py
+++ differentials.py
@@ -14,49 +14,64 @@ def discretise(xa, xb, nx=101, periodic=True):
     eye = np.eye(nx)
 
     # Use central finite differences
-    ddx = (np.roll(eye, 1, 1) - np.roll(eye, -1, 1)) / (2*dx)
-    d2dx2 = (np.roll(eye, 1, 1) - 2*eye + np.roll(eye, -1, 1)) / (dx*dx)
+    ddx = (np.roll(eye, 1, 1) - np.roll(eye, -1, 1)) / (2 * dx)
+    d2dx2 = (np.roll(eye, 1, 1) - 2 * eye + np.roll(eye, -1, 1)) / (dx * dx)
 
     if periodic:
         pass
     else:
         ddx[0, 4:] = 0
-        ddx[0, :4] = (-11/6, 3, -3/2, 1/3) / dx
+        ddx[0, :4] = (-11 / 6, 3, -3 / 2, 1 / 3) / dx
         ddx[-1, :-4] = 0
-        ddx[-1, -4:] = (-1/3, 3/2, -3, 11/6) / dx
+        ddx[-1, -4:] = (-1 / 3, 3 / 2, -3, 11 / 6) / dx
 
         d2dx2[0, 4:] = 0
-        d2dx2[0, :4] = (2, -5, 4, -1) / (dx*dx)
+        d2dx2[0, :4] = (2, -5, 4, -1) / (dx * dx)
         d2dx2[-1, :-4] = 0
-        d2dx2[-1, -4:] = (-1, 4, -5, 2) / (dx*dx)
+        d2dx2[-1, -4:] = (-1, 4, -5, 2) / (dx * dx)
 
     return xs, dx, eye, ddx, d2dx2
 
 
+def d3dx3_mat(nx, dx=1, periodic=True):
+    eye = np.eye(nx)
+    d3dx3 = (
+        0.5 * np.roll(eye, 2, 1)
+        - np.roll(eye, 1, 1)
+        + np.roll(eye, -1, 1)
+        - 0.5 * np.roll(eye, -2, 1)
+    )
+
+    d3dx3 /= dx**3
+
+    if not periodic:
+        raise NotImplementedError  # TODO
+
+    return d3dx3
+
+
 def d4dx4_mat(nx, dx=1, periodic=True):
     eye = np.eye(nx)
-    d4dx4 = (np.roll(eye, 2, 1) - 4*np.roll(eye, 1, 1) + 6*eye
-                 - 4*np.roll(eye, -1, 1) + np.roll(eye, -2, 1)) / dx**4
+    d4dx4 = (
+        np.roll(eye, 2, 1)
+        - 4 * np.roll(eye, 1, 1)
+        + 6 * eye
+        - 4 * np.roll(eye, -1, 1)
+        + np.roll(eye, -2, 1)
+    ) / dx**4
 
     if periodic:
         pass
     else:
-        d4dx4[0, :] = 0;
+        d4dx4[0, :] = 0
         d4dx4[0, :5] = (1, -4, 6, -4, 1) / dx**4
         d4dx4[1, :] = 0
-        d4dx4[1, 1:6] = (1, -4, 6, -4, 1) / dx**4;
-        d4dx4[-2, :] = 0;
+        d4dx4[1, 1:6] = (1, -4, 6, -4, 1) / dx**4
+        d4dx4[-2, :] = 0
         d4dx4[-2, -6:-1] = (1, -4, 6, -4, 1) / dx**4
         d4dx4[-1, :] = 0
-        d4dx4[-1, -5:] = (1, -4, 6, -4, 1) / dx**4;
+        d4dx4[-1, -5:] = (1, -4, 6, -4, 1) / dx**4
 
-    return d4dx4
-
-
-def d4dx4_mat(nx, dx=1):
-    eye = np.eye(nx)
-    d4dx4 = (np.roll(eye, 2, 1) - 4*np.roll(eye, 1, 1) + 6*eye
-                 - 4*np.roll(eye, -1, 1) + np.roll(eye, -2, 1)) / dx**4
     return d4dx4
 
 
